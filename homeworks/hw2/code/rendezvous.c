44180b338e5a491b600692da5fe4c4b0506a5a5f
MhdMartini <mohamed_martini@student.uml.edu>
2022-03-04 13:54:02 -0500

    hw2 part 2 done

--- homeworks/hw2/code/rendezvous.c
+++ homeworks/hw2/code/rendezvous.c
@@ -8,24 +8,43 @@
 
 sem_t s1, s2;
 
-void *child_1(void *arg) {
+void *child_1(void *arg)
+{
     printf("child 1: before\n");
-    // what goes here?
+
+    // signal to the other thread that it can finish
+    sem_post(&s2);
+
+    // wait for the other thread's signal
+    sem_wait(&s1);
+
     printf("child 1: after\n");
     return NULL;
 }
 
-void *child_2(void *arg) {
+void *child_2(void *arg)
+{
     printf("child 2: before\n");
-    // what goes here?
+
+    // signal to the other thread that it can finish
+    sem_post(&s1);
+
+    // wait for the other thread's signal
+    sem_wait(&s2);
+
     printf("child 2: after\n");
     return NULL;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     pthread_t p1, p2;
     printf("parent: begin\n");
+
     // init semaphores here
+    sem_init(&s1, 0, 0);
+    sem_init(&s2, 0, 0);
+
     Pthread_create(&p1, NULL, child_1, NULL);
     Pthread_create(&p2, NULL, child_2, NULL);
     Pthread_join(p1, NULL);
@@ -33,4 +52,3 @@ int main(int argc, char *argv[]) {
     printf("parent: end\n");
     return 0;
 }
-
