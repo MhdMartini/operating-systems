578fa81891db10a030c5bdef15ee31884adf8f04
MhdMartini <mohamed_martini@student.uml.edu>
2022-03-04 16:05:16 -0500

    hw2 part 3 done

--- homeworks/hw2/code/barrier.c
+++ homeworks/hw2/code/barrier.c
@@ -11,42 +11,64 @@
 
 // You likely need two semaphores to do this correctly, and some
 // other integers to track things.
+sem_t s;
+int nWaitingThreads = 0; // number of waiting threads
 
-typedef struct __barrier_t {
-    // add semaphores and other information here
+typedef struct __barrier_t
+{
+    int num_threads;
 } barrier_t;
-
 
 // the single barrier we are using for this program
 barrier_t b;
 
-void barrier_init(barrier_t *b, int num_threads) {
-    // initialization code goes here
+void barrier_init(barrier_t *b, int num_threads)
+{
+    /* initialize semaphor to 0, and store number of
+    threads */
+    sem_init(&s, 0, 0);
+    b->num_threads = num_threads;
 }
 
-void barrier(barrier_t *b) {
-    // barrier code goes here
+void barrier_break(int n_threads)
+{
+    /* when all threads have arrived, reset semaphor to 0
+    to allow all threads to proceed */
+    for (size_t i = 0; i < n_threads; i++)
+        sem_post(&s);
+}
+
+void barrier(barrier_t *b)
+{
+    /* add yourself to waiting list, unless you're the
+    last thread */
+    nWaitingThreads++;
+    if (nWaitingThreads == b->num_threads)
+        barrier_break(b->num_threads);
+    sem_wait(&s);
 }
 
 //
 // XXX: don't change below here (just run it!)
 //
-typedef struct __tinfo_t {
+typedef struct __tinfo_t
+{
     int thread_id;
 } tinfo_t;
 
-void *child(void *arg) {
-    tinfo_t *t = (tinfo_t *) arg;
+void *child(void *arg)
+{
+    tinfo_t *t = (tinfo_t *)arg;
     printf("child %d: before\n", t->thread_id);
     barrier(&b);
     printf("child %d: after\n", t->thread_id);
     return NULL;
 }
 
-
-// run with a single argument indicating the number of 
+// run with a single argument indicating the number of
 // threads you wish to create (1 or more)
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     assert(argc == 2);
     int num_threads = atoi(argv[1]);
     assert(num_threads > 0);
@@ -56,17 +78,17 @@ int main(int argc, char *argv[]) {
 
     printf("parent: begin\n");
     barrier_init(&b, num_threads);
-    
+
     int i;
-    for (i = 0; i < num_threads; i++) {
-	t[i].thread_id = i;
-	Pthread_create(&p[i], NULL, child, &t[i]);
+    for (i = 0; i < num_threads; i++)
+    {
+        t[i].thread_id = i;
+        Pthread_create(&p[i], NULL, child, &t[i]);
     }
 
-    for (i = 0; i < num_threads; i++) 
-	Pthread_join(p[i], NULL);
+    for (i = 0; i < num_threads; i++)
+        Pthread_join(p[i], NULL);
 
     printf("parent: end\n");
     return 0;
 }
-
