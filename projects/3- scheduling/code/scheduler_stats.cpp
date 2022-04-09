bed1b6c75f275e0fa420d919639bd8db98c7f149
MhdMartini <mohamed_martini@student.uml.edu>
2022-04-08 23:17:50 -0400

    changed from using FILE to using ofstream

--- projects/3- scheduling/code/scheduler_stats.cpp
+++ projects/3- scheduling/code/scheduler_stats.cpp
@@ -2,8 +2,67 @@
 #include <iostream>
 #include <stdlib.h>
 #include "scheduler.h"
-
+#include <fstream>
 using namespace std;
+
+// void simulate(char *fileIn, char *fileOut, int interval)
+// {
+//     Requests reqQue(fileIn);
+//     FCFS fcfs(&reqQue);
+//     Scheduler sch(&fcfs);
+
+//     FILE *fp = fopen(fileOut, "w");
+//     fprintf(fp, "***** FCFS Scheduling *****\n");
+
+//     Process *currP = NULL;
+//     Process *lastP = NULL;
+//     int t = 30;
+//     bool done = false;
+//     while (!sch.step())
+//     {
+//         t = sch.rQ->reqQue->t - 1;
+
+//         lastP = currP;
+//         currP = sch.currProcess;
+
+//         if (t % interval)
+//             continue;
+
+//         fprintf(fp, "t = %d\n", t);
+//         if (lastP != currP)
+//         {
+//             // loading / finishing / preempting
+//             if (lastP == NULL)
+//                 // loading
+//                 fprintf(fp, "CPU: Loading process %d (CPU burst = %d)\n", currP->id, currP->tb);
+//             else
+//             {
+//                 // finishing / preempting
+//                 if (lastP->taub > 0)
+//                 {
+//                     // preempting
+//                     fprintf(fp, "CPU: Preempting process %d (remaining CPU burst = %d); ", lastP->id, lastP->taub);
+//                     fprintf(fp, "loading process %d (CPU burst = %d)\n", currP->id, currP->tb);
+//                 }
+//                 else
+//                 {
+//                     // finishing and loading
+//                     fprintf(fp, "CPU: Finishing process %d; ", lastP->id);
+//                     fprintf(fp, "loading process %d (CPU burst = %d)\n", currP->id, currP->tb);
+//                 }
+//             }
+//         }
+//         else
+//             // running
+//             fprintf(fp, "CPU: Running process %d (remaining CPU burst = %d)\n", currP->id, currP->taub + 1);
+//         fprintf(fp, "\n");
+//     }
+//     fprintf(fp, "t = %d\n", ++t);
+//     fprintf(fp, "CPU: Finishing process %d ", currP->id);
+
+//     cout << "Good bye!" << endl;
+//     fclose(fp);
+// }
 
 void simulate(char *fileIn, char *fileOut, int interval)
 {
@@ -11,49 +70,56 @@ void simulate(char *fileIn, char *fileOut, int interval)
     FCFS fcfs(&reqQue);
     Scheduler sch(&fcfs);
 
-    FILE *fp = fopen(fileOut, "a");
+    ofstream myfile;
+    myfile.open(fileOut);
+    myfile << "***** FCFS Scheduling *****" << endl;
 
     Process *currP = NULL;
     Process *lastP = NULL;
+    int t = 30;
+    bool done = false;
     while (!sch.step())
     {
-        fprintf(fp, "t = %d\n", sch.rQ->reqQue->t - 1);
+        t = sch.rQ->reqQue->t - 1;
 
         lastP = currP;
         currP = sch.currProcess;
 
+        if (t % interval)
+            continue;
+
+        myfile << "t = " << t << endl;
         if (lastP != currP)
         {
             // loading / finishing / preempting
             if (lastP == NULL)
-            {
                 // loading
-                fprintf(fp, "CPU: Loading process %d (CPU burst = %d)\n", currP->id, currP->tb);
-            }
+                myfile << "CPU: Loading process " << currP->id << " (CPU burst = " << currP->tb << ")" << endl;
             else
             {
                 // finishing / preempting
                 if (lastP->taub > 0)
                 {
                     // preempting
-                    fprintf(fp, "CPU: Preempting process %d (remaining CPU burst = %d); ", lastP->id, lastP->taub);
-                    fprintf(fp, "loading process %d (CPU burst = %d)\n", currP->id, currP->tb);
+                    myfile << "CPU: Preempting process " << lastP->id << " (remaining CPU burst = " << lastP->taub << "); ";
+                    myfile << "loading process " << currP->id << " (CPU burst = " << currP->tb << ")" << endl;
                 }
                 else
                 {
-                    // finishing
-                    fprintf(fp, "CPU: Finishing process %d (remaining CPU burst = %d); ", lastP->id, lastP->taub);
-                    fprintf(fp, "loading process %d (CPU burst = %d)\n", currP->id, currP->tb);
+                    // finishing and loading
+                    myfile << "CPU: Finishing process " << lastP->id << "; ";
+                    myfile << "loading process " << currP->id << " (CPU burst = " << currP->tb << ")" << endl;
                 }
             }
         }
         else
-        {
             // running
-            fprintf(fp, "CPU: Running process %d (remaining CPU burst = %d)\n", currP->id, currP->taub + 1);
-        }
-        fprintf(fp, "\n");
+            myfile << "CPU: Running process " << currP->id << " (remaining CPU burst = " << currP->taub + 1 << ")" << endl;
+        myfile << endl;
     }
-    sch.display();
-    fclose(fp);
+    myfile << "t = " << ++t << endl;
+    myfile << "CPU: Finishing process " << currP->id << endl;
+
+    cout << "Good bye!" << endl;
+    myfile.close();
 }
