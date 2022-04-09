3b8ab9b6b326f428e77d0a4cc63a2db9572252f7
MhdMartini <mohamed_martini@student.uml.edu>
2022-04-08 23:18:56 -0400

    pipeline complete except from ready que representation

--- projects/3- scheduling/code/scheduler_stats.cpp
+++ projects/3- scheduling/code/scheduler_stats.cpp
@@ -5,65 +5,6 @@
 #include <fstream>
 using namespace std;
 
-// void simulate(char *fileIn, char *fileOut, int interval)
-// {
-//     Requests reqQue(fileIn);
-//     FCFS fcfs(&reqQue);
-//     Scheduler sch(&fcfs);
-
-//     FILE *fp = fopen(fileOut, "w");
-//     fprintf(fp, "***** FCFS Scheduling *****\n");
-
-//     Process *currP = NULL;
-//     Process *lastP = NULL;
-//     int t = 30;
-//     bool done = false;
-//     while (!sch.step())
-//     {
-//         t = sch.rQ->reqQue->t - 1;
-
-//         lastP = currP;
-//         currP = sch.currProcess;
-
-//         if (t % interval)
-//             continue;
-
-//         fprintf(fp, "t = %d\n", t);
-//         if (lastP != currP)
-//         {
-//             // loading / finishing / preempting
-//             if (lastP == NULL)
-//                 // loading
-//                 fprintf(fp, "CPU: Loading process %d (CPU burst = %d)\n", currP->id, currP->tb);
-//             else
-//             {
-//                 // finishing / preempting
-//                 if (lastP->taub > 0)
-//                 {
-//                     // preempting
-//                     fprintf(fp, "CPU: Preempting process %d (remaining CPU burst = %d); ", lastP->id, lastP->taub);
-//                     fprintf(fp, "loading process %d (CPU burst = %d)\n", currP->id, currP->tb);
-//                 }
-//                 else
-//                 {
-//                     // finishing and loading
-//                     fprintf(fp, "CPU: Finishing process %d; ", lastP->id);
-//                     fprintf(fp, "loading process %d (CPU burst = %d)\n", currP->id, currP->tb);
-//                 }
-//             }
-//         }
-//         else
-//             // running
-//             fprintf(fp, "CPU: Running process %d (remaining CPU burst = %d)\n", currP->id, currP->taub + 1);
-//         fprintf(fp, "\n");
-//     }
-//     fprintf(fp, "t = %d\n", ++t);
-//     fprintf(fp, "CPU: Finishing process %d ", currP->id);
-
-//     cout << "Good bye!" << endl;
-//     fclose(fp);
-// }
-
 void simulate(char *fileIn, char *fileOut, int interval)
 {
     Requests reqQue(fileIn);
