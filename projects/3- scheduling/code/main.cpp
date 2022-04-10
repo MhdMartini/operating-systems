7bb78e53c2e5305f668df5f145530772fd0344c3
MhdMartini <mohamed_martini@student.uml.edu>
2022-04-10 13:14:04 -0400

    added functions to simulate the different algorithms

--- projects/3- scheduling/code/main.cpp
+++ projects/3- scheduling/code/main.cpp
@@ -1,33 +1,9 @@
-/*
-Process
-    - holds process-related information
-    - process helpful functions:
-        * burst
-        * wait
-
-PNode
-    - process node class to hold a process and point to a next process
-    - used by requests que to sort requests by arrival time
-
-Requests
-    - reads and stores all requests from file
-    - sorts the requests according to arrival time as a linked list
-    - step method:
-        updates the end and len attributes:
-            * end tells us how many requests to read at a give time
-            * len tells us how many requests remain.
-
-ReadyQue
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 #include <string>
 #include <assert.h>
-#include "process.h"
-#include "scheduler.h"
+#include "simulate.h"
 
 using namespace std;
 
@@ -37,23 +13,7 @@ int main(int argc, char *argv[])
     char *fileOut = argv[2];
     int interval = atoi(argv[3]);
 
-    Scheduler sch(fileIn, "FCFS");
-    ofstream myfile;
-    myfile.open(fileOut);
-    myfile << "***** FCFS Scheduling *****\n";
-    bool done = false;
-    int t = 0;
-    do
-    {
-        done = sch.step();
-        if (!(t % interval))
-            sch.report(&myfile);
-        t++;
-    } while (!done);
-    myfile << "\n*********************************************************\n";
-    sch.summary(&myfile);
-
-    myfile.close();
+    simulateFCFS(fileIn, fileOut, interval);
 
     return 0;
 }
