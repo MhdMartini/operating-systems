3f59ab0d2a05a8c4fae1a3d23fb421c54bbb19aa
MhdMartini <mohamed_martini@student.uml.edu>
2022-04-10 11:44:07 -0400

    refactoring

--- projects/3- scheduling/code/main.cpp
+++ projects/3- scheduling/code/main.cpp
@@ -1,26 +1,3 @@
-/*
-Process
-    - holds process-related information
-    - process helpful functions:
-        * burst
-        * wait
-
-PNode
-    - process node class to hold a process and point to a next process
-    - used by requests que to sort requests by arrival time
-
-Requests
-    - reads and stores all requests from file
-    - sorts the requests according to arrival time as a linked list
-    - step method:
-        updates the end and len attributes:
-            * end tells us how many requests to read at a give time
-            * len tells us how many requests remain.
-
-ReadyQue
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
